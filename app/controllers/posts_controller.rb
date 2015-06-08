6bdacb3f0eef789c72386dba1987ca2efcea5399
Sourav Moitra <sourav.moitr@gmail.com>
2015-06-09 03:06:11 +0530

    Bootstrapping posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  http_basic_authenticate_with name: "cpg", password: "password", except: :index
+  http_basic_authenticate_with name: "cpg", password: "password", except: [:index, :show]
 
 
   def index
