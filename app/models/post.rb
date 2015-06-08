a432a8a4965ebc1f0b035fda518f42ac36af042c
Sourav Moitra <sourav.moitr@gmail.com>
2015-06-08 19:37:18 +0530

    posts model complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  belongs_to :category
+
+  attr_accessor :category_string
+
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :body, presence: true
+
+  before_validation :set_category
+
+  protected
+
+  def set_category
+    self.category = Category.find_or_create_by(title: :category_string)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608095059) do
+ActiveRecord::Schema.define(version: 20150608132540) do
 
-  create_table "posts", force: :cascade do |t|
+  create_table "categories", force: :cascade do |t|
     t.string   "title"
-    t.string   "category"
-    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.integer  "category_id"
+  end
+
+  add_index "posts", ["category_id"], name: "index_posts_on_category_id"
 
 end
