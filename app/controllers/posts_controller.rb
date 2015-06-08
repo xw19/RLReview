74ae5e0c5450e22c7b1d3a90aeea03ac06ec0901
Sourav Moitra <sourav.moitr@gmail.com>
2015-06-08 21:23:20 +0530

    Controller and views for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,54 @@
 class PostsController < ApplicationController
+  http_basic_authenticate_with name: "cpg", password: "password", except: :index
+
+
+  def index
+    @posts = Post.all
+  end
+
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(posts_params)
+    @post.user_id = 1 # Currently there is only one author
+    if @post.save
+      redirect_to @post, notice: "Post successfully created"
+    else
+      flash.now[:alert] = "Somethng went wrong"
+      render 'new'
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(posts_params)
+      redirect_to @post, notice: "Post successfully updated"
+    else
+      flash.now[:alert] = "Somethng went wrong"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.delete
+    redirect_to post_index_path, notice: "Post has been deleted"
   end
 
   def show
+    @post = Post.find(params[:id])
   end
+
+  private
+
+  def posts_params
+    params.require(:post).permit(:title, :category_string, :body)
+  end
+
 end
