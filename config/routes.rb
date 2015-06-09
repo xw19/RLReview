cbb881918cc3647a187b8f6c8e99ae4c0f8f188d
Sourav Moitra <sourav.moitr@gmail.com>
2015-06-09 20:18:52 +0530

    routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts do
-    resources :comments, only: [:create, :destroy]      
-  end
+  resources :posts
+  resources :comments, only: [:create, :destroy] 
   root 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
