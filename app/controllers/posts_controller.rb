4c2252cfd078333229102ab85ae43927a49dd5fa
Sourav Moitra <sourav.moitr@gmail.com>
2015-06-10 08:11:47 +0530

    Added kaminari pagination to comments and markup improvement

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -43,6 +43,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @comments = @post.comments.page(params[:page])
   end
 
   private
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
+  paginates_per 5
   belongs_to :commentable, polymorphic: true
   belongs_to :post
   belongs_to :comment
