e69a91290d34a5d68c32f9b9f3858de77a77f98c
Sourav Moitra <sourav.moitr@gmail.com>
2015-06-11 02:11:31 +0530

    Redcarpet now process the merkdown

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new(no_intra_emphasis: true, hard_wrap: true, autolink: true)
+    markdown = Redcarpet::Markdown.new(renderer, autolink: true, tables: true, safe_links_only: true, fenced_code_blocks: true)
+    markdown.render(text).html_safe
+  end
+
+
 end
