e12e2dd1d423538006c27ba5d816b05a80a29c75
Sourav Moitra <sourav.moitr@gmail.com>
2015-06-12 12:21:07 +0530

    rails back method used instead of session based redirect

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
 
   def create
-    session[:return_to] ||= request.referer
     @comment = Comment.new(comments_params)
     @comment.user_id = 1 # to be replaced with current_user
     if @comment.save
@@ -9,7 +8,7 @@ class CommentsController < ApplicationController
     else
       flash[:alert] = "something went wrong"
     end
-    redirect_to session.delete(:return_to)
+    redirect_to :back
   end
 
 
