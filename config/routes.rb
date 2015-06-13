7431fc55568ff85a8e08efa4ba2db48dbbc8a1e7
Sourav Moitra <sourav.moitr@gmail.com>
2015-06-13 19:49:22 +0530

    reporting can be done when logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts
-  resources :comments, only: [:create, :destroy] 
+  resources :comments, only: [:create, :destroy]
+  resources :reports, only: [:create] 
   root 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
